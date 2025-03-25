4409a2a2eb9cf8a40406487bc595b15ac2fe71b0
makifcevik <makif.cevik02@gmail.com>
2025-03-25 17:11:17 +0300

    Install and configure TanStack Query, Axios

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.tsx";
 import "@fontsource/poppins";
@@ -7,12 +8,24 @@ import "@fontsource/roboto";
 import { BrowserRouter } from "react-router-dom";
 import "./utils/i18n/index.ts";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutes caching
+      retry: 2,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 const rootElement = document.getElementById("root") as HTMLElement;
 
 createRoot(rootElement).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
