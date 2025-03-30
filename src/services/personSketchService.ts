e6ca539434ac9fbba69776b259a1e8c528759392
makifcevik <makif.cevik02@gmail.com>
2025-03-30 19:47:10 +0300

    Implement CRUD operations on personSketchService

--- src/services/personSketchService.ts
+++ src/services/personSketchService.ts
@@ -8,3 +8,35 @@ export const getAllPersonSketches = async (): Promise<IPersonSketch[]> => {
   );
   return response.data;
 };
+
+export const getPersonSketch = async (id: number): Promise<IPersonSketch> => {
+  const response = await axiosInstance.get<IPersonSketch>(
+    `${API_ROUTES.PERSON_SKETCHES}/${id}`
+  );
+  return response.data;
+};
+
+export const createPersonSketch = async (
+  newSketch: IPersonSketch
+): Promise<IPersonSketch> => {
+  const response = await axiosInstance.post<IPersonSketch>(
+    API_ROUTES.PERSON_SKETCHES,
+    newSketch
+  );
+  return response.data;
+};
+
+export const updatePersonSketch = async (
+  id: number,
+  sketchData: Partial<IPersonSketch>
+): Promise<IPersonSketch> => {
+  const response = await axiosInstance.put<IPersonSketch>(
+    `${API_ROUTES.PERSON_SKETCHES}/${id}`,
+    sketchData
+  );
+  return response.data;
+};
+
+export const deletePersonSketch = async (id: number): Promise<void> => {
+  await axiosInstance.delete(`${API_ROUTES.PERSON_SKETCHES}/${id}`);
+};
