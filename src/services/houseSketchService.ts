b4ae0bdda5b99985e780a194d8f00e380cab6de8
makifcevik <makif.cevik02@gmail.com>
2025-03-30 19:47:56 +0300

    Implement CRUD operations on houseSketchService

--- src/services/houseSketchService.ts
+++ src/services/houseSketchService.ts
@@ -8,3 +8,35 @@ export const getAllHouseSketches = async (): Promise<IHouseSketch[]> => {
   );
   return response.data;
 };
+
+export const getHouseSketch = async (id: number): Promise<IHouseSketch> => {
+  const response = await axiosInstance.get<IHouseSketch>(
+    `${API_ROUTES.HOUSE_SKETCHES}/${id}`
+  );
+  return response.data;
+};
+
+export const createHouseSketch = async (
+  newSketch: IHouseSketch
+): Promise<IHouseSketch> => {
+  const response = await axiosInstance.post<IHouseSketch>(
+    API_ROUTES.HOUSE_SKETCHES,
+    newSketch
+  );
+  return response.data;
+};
+
+export const updateHouseSketch = async (
+  id: number,
+  sketchData: Partial<IHouseSketch>
+): Promise<IHouseSketch> => {
+  const response = await axiosInstance.put<IHouseSketch>(
+    `${API_ROUTES.HOUSE_SKETCHES}/${id}`,
+    sketchData
+  );
+  return response.data;
+};
+
+export const deleteHouseSketch = async (id: number): Promise<void> => {
+  await axiosInstance.delete(`${API_ROUTES.HOUSE_SKETCHES}/${id}`);
+};
