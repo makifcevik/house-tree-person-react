634f6ff0499d99792faaed6891e8775b62673ea2
makifcevik <makif.cevik02@gmail.com>
2025-03-30 19:29:43 +0300

    Implement CRUD operations on treeSketchService

--- src/services/treeSketchService.ts
+++ src/services/treeSketchService.ts
@@ -6,3 +6,33 @@ export const getAllTreeSketches = async (): Promise<ITreeSketch[]> => {
   const response = await axiosInstance.get<ITreeSketch[]>(`${API_ROUTES.TREE_SKETCHES}`);
   return response.data;
 };
+
+export const getTreeSketch = async (id: number): Promise<ITreeSketch> => {
+  const response = await axiosInstance.get<ITreeSketch>(
+    `${API_ROUTES.TREE_SKETCHES}/${id}`
+  );
+  return response.data;
+};
+
+export const createTreeSketch = async (newSketch: ITreeSketch): Promise<ITreeSketch> => {
+  const response = await axiosInstance.post<ITreeSketch>(
+    API_ROUTES.TREE_SKETCHES,
+    newSketch
+  );
+  return response.data;
+};
+
+export const updateTreeSketch = async (
+  id: number,
+  sketchData: Partial<ITreeSketch>
+): Promise<ITreeSketch> => {
+  const response = await axiosInstance.put<ITreeSketch>(
+    `${API_ROUTES.TREE_SKETCHES}/${id}`,
+    sketchData
+  );
+  return response.data;
+};
+
+export const deleteTreeSketch = async (id: number): Promise<void> => {
+  await axiosInstance.delete(`${API_ROUTES.TREE_SKETCHES}/${id}`);
+};
